de3b7ea88e6f1718fbba6260b18f93d75602e768
Wan Anis <wananisjo@gmail.com>
2021-10-01 20:57:41 +0800

    pswd check

--- event-handler/js/event.js
+++ event-handler/js/event.js
@@ -2,7 +2,25 @@ function transformUppercase(_val){ //function expextng value
     return _val.toUpperCase();
 }
 
+// DOM event listener
 let elNamaPengguna = document.getElementById('namaPengguna'); //ingt getElementById
 elNamaPengguna.addEventListener("keyup", function(){
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
-})
+}) //attaching event listener 1st type
+
+function  checkPassword(){
+    //fetch element first
+    let elKataLaluan = document.getElementById("kataLaluan")
+    let pass_val = elKataLaluan.value
+
+    //check length pswd
+    if(pass_val.length>5){
+        alert("Password check...OK!")
+    }else {
+        alert("Password too short ma!")
+    }
+}
+
+//tradional DOM event handler
+let elKl = document.getElementById("kataLaluan")
+elKl.onblur = checkPassword //onblur bila offfocus bila tekan kt luar pswd area tu dia check
