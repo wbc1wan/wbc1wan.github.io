072a2502fa1187941735a60f6d9527a448282ab6
Wan Anis <wananisjo@gmail.com>
2021-10-11 21:55:58 +0800

    add delete function by api request

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -13,21 +13,24 @@ function GetBooking() {
         .then(json => {
             // Do something with the data
             console.log(json.bookings);
+
             // below is add edited 
             let bookingNameList = document.getElementById("bookingNameList")
+            let bookingIds = []
 
             //delete all rows in the table
-            for(let k = bookingNameList.rows.length - 1; k > 0; k--){
+            for (let k = bookingNameList.rows.length - 1; k > 0; k--) {
                 bookingNameList.deleteRow(k)
             }
 
             //load all rows from Sheety API
-            for(let i = 0; i <json.bookings.length; i++){
+            for (let i = 0; i < json.bookings.length; i++) {
                 let gName = json.bookings[i].name;
                 let gEmail = json.bookings[i].email;
                 let gPax = json.bookings[i].pax;
                 let gRemarks = json.bookings[i].remarks;
                 let gId = json.bookings[i].id;
+                let btnId = "delete" + gId;
 
                 let row = bookingNameList.insertRow(bookingNameList.rows.length)
                 row.insertCell(0).innerHTML = gId
@@ -35,7 +38,32 @@ function GetBooking() {
                 row.insertCell(2).innerHTML = gEmail
                 row.insertCell(3).innerHTML = gPax
                 row.insertCell(4).innerHTML = gRemarks
-                row.insertCell(5).innerHTML = ""
+                row.insertCell(5).innerHTML = "<button id='" + btnId + "' type='button' class='btn btn-danger'>Delete</button>"
+
+                bookingIds.push(btnId)
             }
+
+            for (let j = 0; j < bookingIds.length; j++) {
+                let el = document.getElementById(bookingIds[j])
+                el.addEventListener("click", function () {
+                    let theId = el.id.replace("delete", "")
+                    //console.log(theId)
+                    DeleteBooking(theId)
+                })
+            }
+
+
+        });
+}
+
+function DeleteBooking(id) {
+    let url = 'https://api.sheety.co/e605e885f5f282dff0485448c92b7ff3/bookingApp/bookings/' + id;
+    fetch(url, {
+        method: 'DELETE',
+    })
+        //.then((response) => response.json()) no need sbb server x return in json so delete this line
+        .then(() => {
+            alert("Record id " + id + " deleted!")
+            GetBooking()
         });
 }
