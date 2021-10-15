c26c9259f3088ce5bb3832dec2f0f3aa18567b9d
Wan Anis <wananisjo@gmail.com>
2021-10-16 01:48:10 +0800

    css palindrome

---
+++ asg4/js/index.js
@@ -0,0 +1,28 @@
+var checkButton = document.getElementById("checkPalindrome");
+
+function isPalindrome(str) {
+  'use strict';
+  
+  str = str.replace(/[^a-z0-9]+/gi, "").toLowerCase();
+  var reversedStr = str.split("").reverse().join("");
+  console.log(reversedStr);
+  document.getElementById("res").innerHTML = "Inversed is (" + reversedStr + ")" ;
+
+  return str === reversedStr;
+  
+}
+
+checkButton.addEventListener("click", function() {
+  'use strict';
+  
+  var value = document.getElementById("inputPalindrome").value;
+  var notification = document.getElementById("notification");
+  
+  if(isPalindrome(value)) {
+    notification.innerHTML = "Yay! You've got a palindrome";
+    notification.className = "alert alert-success";
+  } else {
+    notification.innerHTML = "Ops! Not a palindrome";
+    notification.className = "alert alert-danger";
+  }
+});
